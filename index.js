0551aefdf475a738318bf7764f3ab8a03f915718
Ali <105407757+alizafar09@users.noreply.github.com>
2022-09-12 10:56:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import ComA from './context/comA';
 
 const Component = () =>{
   const[value, setValue] = useState('');
@@ -17,10 +16,3 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
  root.render(
      <Component />
  );
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <ComA />
-//   </React.StrictMode>
-// );
